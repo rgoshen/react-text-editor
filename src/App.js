c531872993c1b48b812bdd644f52490da2e7833f
Richard Goshen <rick.goshen@gmail.com>
2023-02-08 19:25:14 -0700

    add basic functionality to editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { EditorState } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import { convertToHTML } from 'draft-convert';
+
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
+import DOMPurify from 'dompurify';
 
 function App() {
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  const [convertedContent, setConvertedContent] = useState(null);
+
+  useEffect(() => {
+    let html = convertToHTML(editorState.getCurrentContent());
+    setConvertedContent(html);
+  }, [editorState]);
+
+  console.log(convertedContent);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'>Rich Text Editor</header>
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={setEditorState}
+        wrapperClassName='wrapper-class'
+        editorClassName='editor-class'
+        toolbarClassName='toolbar-class'
+        toolbar={{
+          options: [
+            'inline',
+            'blockType',
+            'fontSize',
+            'fontFamily',
+            'list',
+            'textAlign',
+            'colorPicker',
+            'link',
+            'embedded',
+            'emoji',
+            'image',
+            'remove',
+            'history',
+          ],
+        }}
+        hashtag={{
+          separator: ' ',
+          trigger: '#',
+        }}
+        mention={{
+          separator: ' ',
+          trigger: '@',
+          suggestions: [
+            { text: 'JavaScript', value: 'javascript', url: 'js' },
+            { text: 'Golang', value: 'golang', url: 'go' },
+          ],
+        }}
+      />
+      <div
+        className='preview'
+        dangerouslySetInnerHTML={createMarkup(convertedContent)}
+      ></div>
     </div>
   );
+}
+
+function createMarkup(html) {
+  return {
+    __html: DOMPurify.sanitize(html),
+  };
 }
 
 export default App;
